c7627edaaff48557140db0422d5963b74d897d49
Arne Beer <arne@twobeer.de>
2017-04-22 20:01:36 +0200

    Use bytea for password

--- migrations/20170211131857_create_initial_db/up.sql
+++ migrations/20170211131857_create_initial_db/up.sql
@@ -5,6 +5,6 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT current_timestamp NOT NULL,
     updated_at TIMESTAMP DEFAULT current_timestamp NOT NULL,
     email VARCHAR(120) UNIQUE NOT NULL,
-    password_hash VARCHAR(120) NOT NULL
+    password_hash BYTEA NOT NULL
 );
 SELECT diesel_manage_updated_at('users');
